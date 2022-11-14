897e8826ec2f20b78d94fb9cc763739fed3d5ccb
PauliVr <paulette.vr.10@gmail.com>
2022-11-14 09:17:53 -0600

    Adds router and components

---
+++ src/main.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Layout from './components/Layout';
+import './index.css';
+import Index, { loader as clientesLoader } from './pages/Index';
+import NuevoCliente from './pages/NuevoCliente';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <Index />,
+        loader: clientesLoader,
+      },
+      {
+        path: '/clientes/nuevo',
+        element: <NuevoCliente />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
