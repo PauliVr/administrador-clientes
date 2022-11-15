447b38068d282978c87f60bc9ececb28cbfe2408
PauliVr <paulette.vr.10@gmail.com>
2022-11-15 08:42:40 -0600

    Create the title and back button to new client form

--- src/pages/NuevoCliente.jsx
+++ src/pages/NuevoCliente.jsx
@@ -1,8 +1,27 @@
+import { useNavigate } from 'react-router-dom';
 
 function NuevoCliente() {
+  const navigate = useNavigate();
+
   return (
-    <div>NuevoCliente</div>
-  )
+    <>
+      <h1 className='font-black text-4xl text-blue-900'>Nuevo Cliente</h1>
+      <p className='mt-3'>Llena todos los campos para registrar un nuevo cliente</p>
+
+      <div className='flex justify-end'>
+        <button
+          className='bg-blue-800 text-white px-3 py-1 font-bold uppercase'
+          onClick={() => navigate(-1)}
+        >
+          Volver
+        </button>
+      </div>
+
+      <div className='bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10'>
+        <p>Formulario aquí</p>
+      </div>
+    </>
+  );
 }
 
-export default NuevoCliente
+export default NuevoCliente;
