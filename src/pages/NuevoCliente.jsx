f062a6d1fa2ffdbc43d8ecdc0453325cdb295483
PauliVr <paulette.vr.10@gmail.com>
2022-11-15 08:49:27 -0600

    Adds the form component and submit button in new client component

--- src/pages/NuevoCliente.jsx
+++ src/pages/NuevoCliente.jsx
@@ -1,4 +1,5 @@
 import { useNavigate } from 'react-router-dom';
+import Formulario from '../components/Formulario';
 
 function NuevoCliente() {
   const navigate = useNavigate();
@@ -17,8 +18,15 @@ function NuevoCliente() {
         </button>
       </div>
 
-      <div className='bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10'>
-        <p>Formulario aquí</p>
+      <div className='bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10 mt-20'>
+        <form>
+          <Formulario />
+          <input
+            type='submit'
+            className='mt-5 w-full bg-blue-800 p-3 uppercase font-bold text-white text-lg'
+            value='registrar cliente'
+          />
+        </form>
       </div>
     </>
   );
