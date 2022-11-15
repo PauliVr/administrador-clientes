4232d8bced82524381dfa4bac7331391088c5cdc
PauliVr <paulette.vr.10@gmail.com>
2022-11-15 08:31:29 -0600

    Adds client data from object and add styles

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,3 +1,7 @@
+import { useLoaderData } from 'react-router-dom';
+import Cliente from '../components/Cliente';
+
+// Exportar Loader
 export function loader() {
   const clientes = [
     {
@@ -40,10 +44,32 @@ export function loader() {
 }
 
 function Index() {
+  // Recuperamos Loader
+  const clientes = useLoaderData();
+
   return (
     <>
       <h1 className='font-black text-4xl text-blue-900'>Clientes</h1>
       <p className='mt-3'>Administra tus clientes</p>
+
+      {clientes.length ? (
+        <table className='w-full bg-white shadow mt-5 table-auto'>
+          <thead className='bg-blue-800 text-white'>
+            <tr>
+              <th className='p-2'>Clientes</th>
+              <th className='p-2'>Contacto</th>
+              <th className='p-2'>Acciones</th>
+            </tr>
+          </thead>
+          <tbody>
+            {clientes.map((cliente) => (
+              <Cliente key={cliente.id} cliente={cliente} />
+            ))}
+          </tbody>
+        </table>
+      ) : (
+        <p className='text-center mt-10'>No hay clientes aún</p>
+      )}
     </>
   );
 }
